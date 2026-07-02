08d59592120016565a80c2ef52ee462b3321f6b0
sukiinsight <seojeongchoi8@gmail.com>
2026-07-02 21:39:20 +0900

    Update index.jsExit after pending updates are processed

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import { createServer } from "node:http";
-import { bot } from "./telegram.js";
+import { bot, processPendingUpdates } from "./telegram.js";
 import { config } from "./config.js";
 
 process.once("SIGINT", () => bot.stop("SIGINT"));
@@ -8,6 +8,12 @@ process.once("SIGTERM", () => bot.stop("SIGTERM"));
 function sendHealthResponse(res, mode) {
   res.writeHead(200, { "content-type": "application/json" });
   res.end(JSON.stringify({ ok: true, mode }));
+}
+
+if (process.env.PROCESS_PENDING_UPDATES === "true") {
+  await processPendingUpdates();
+  console.log("Finished processing pending Telegram updates");
+  process.exit(0);
 }
 
 if (config.PUBLIC_WEBHOOK_URL) {
